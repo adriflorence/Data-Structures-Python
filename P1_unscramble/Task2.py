c5cde805b154bd401af61a8d157fe7d67aacab53
Adri Forczek <forczekadri@gmail.com>
2020-03-03 23:01:26 +0000

    chore: calculates longest convo between parties

--- P1_unscramble/Task2.py
+++ P1_unscramble/Task2.py
@@ -16,3 +16,18 @@ Print a message:
 September 2016.".
 """
 
+def calculateLongestConverstation(calls):
+    longest = calls[0]
+    for call in calls:
+        if(int(call[-1]) > int(longest[-1])):
+            longest = call
+    return longest
+
+def printLongestConversation(longest):
+    number = longest[0]
+    duration = longest[-1]
+    answer = "{} spent the longest time, {} seconds, on the phone during September 2016.".format(number, duration)
+    print(answer)
+
+longest = calculateLongestConverstation(calls)
+printLongestConversation(longest)
