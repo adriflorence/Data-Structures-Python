739590843ae880dfa8b458179c007fcede3ce86b
Adri Forczek <forczekadri@gmail.com>
2020-03-06 00:27:04 +0000

    chore: calculates telemarketers (task 4)

--- P1_unscramble/Task4.py
+++ P1_unscramble/Task4.py
@@ -1,5 +1,6 @@
 import csv
 
+# Read from files
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
@@ -8,16 +9,58 @@ with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
-"""
-TASK 4:
-The telephone company want to identify numbers that might be doing
-telephone marketing. Create a set of possible telemarketers:
-these are numbers that make outgoing calls but never send texts,
-receive texts or receive incoming calls.
 
-Print a message:
-"These numbers could be telemarketers: "
-<list of numbers>
-The list of numbers should be print out one per line in lexicographic order with no duplicates.
-"""
+# Returns a list of calling telephone numbers with no duplicates
+def telephoneNumbers(calls, index):
+    result = set()
+    for call in calls:
+        result.add(call[index])
+    return result
 
+# Returns a list of calling telephone numbers with no duplicates
+def mobileNumbers(texts):
+    result = set()
+    for text in texts:
+        result.add(text[0])
+        result.add(text[1])
+    return result
+
+
+# Returns a list of callers that never had incoming calls
+def noIncomingCalls(callers, callees):
+    result = set()
+    for caller in callers:
+        if(caller not in callees):
+            result.add(caller)
+    return result
+
+# Returns a list of caller numbers that does NOT appear in the texts.csv file
+def noTexts(callers, mobiles):
+    result = set()
+    # print(len(callers))
+    for caller in callers:
+        if(caller not in mobiles):
+            result.add(caller)
+    return result
+
+def printTelemarketers(telemarketers):
+    for tm in telemarketers:
+        print(tm)
+
+# The telephone company want to identify numbers that might be doing
+# telephone marketing. Create a set of possible telemarketers:
+# these are numbers that make outgoing calls but never send texts,
+# receive texts or receive incoming calls.
+
+
+callers = list(telephoneNumbers(calls, 0))
+callees = list(telephoneNumbers(calls, 1))
+mobiles = list(mobileNumbers(texts))
+# print(len(callers))
+# print(len(callees))
+# print(len(mobiles))
+
+callers_without_incoming_calls = noIncomingCalls(callers, callees)
+callers_without_texts = noTexts(callers_without_incoming_calls, mobiles)
+telemarketers = sorted(list(callers_without_texts))
+printTelemarketers(telemarketers)
